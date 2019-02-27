c2906f5371b1dbfefa110257d2610922971e1413
Hero X <haoy@zmjiudian.com>
2019-02-27 16:32:06 +0800

    初始项目代码

---
+++ pages/index/index.vue
@@ -0,0 +1,45 @@
+<template>
+	<view class="content">
+        <image class="logo" src="../../static/logo.png"></image>
+		<view>
+            <text class="title">{{title}} 你好 世界</text>
+        </view>
+		<view>
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400upx;
+	}
+    .logo{
+        height: 200upx;
+        width: 200upx;
+        margin-top: 200upx;
+    }
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+</style>
